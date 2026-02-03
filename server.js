6d4dc8dc6493de99670f5ff678b342e768874915
Masade Osahon Paul <paulmasade@gmail.com>
2026-02-03 11:15:54 +0100

    Fix CORS for Vercel deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "*", 
+  origin: "https://elearning-gamified.vercel.app", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
